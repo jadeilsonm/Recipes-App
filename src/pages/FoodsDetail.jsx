2e6be448a2c01b34a07740ad5e846850f7a62bec
Franklin Ramos <franklinramos@outlook.com>
2022-04-01 17:58:11 -0300

    Linter

--- src/pages/FoodsDetail.jsx
+++ src/pages/FoodsDetail.jsx
@@ -5,7 +5,7 @@ import Button from '../components/Button';
 import shareIcon from '../images/shareIcon.svg';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
-import CarouselRecomendation from '../components/CarouselRecomendation';
+// import CarouselRecomendation from '../components/CarouselRecomendation';
 
 export default function FoodsDetail() {
   const location = useLocation();
@@ -197,49 +197,16 @@ export default function FoodsDetail() {
         src={ foodDetail.strYoutube }
       />
       <h3>Recommended</h3>
-      {drinkRecomendation.length && (
-        <CarouselRecomendation drink={ drinkRecomendation } />
-      )}
-      {/* <div
-        id="carouselExampleSlidesOnly"
-        className="carousel slide"
-        data-ride="carousel"
-      >
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img
-              src={ drinkRecomendation[0].strDrinkThumb }
-              alt={ drinkRecomendation[0].strDrink }
-              className="d-block w-100"
-            />
-          </div>
-          <div className="carousel-item">
-            <img
-              src={ drinkRecomendation[1].strDrinkThumb }
-              alt={ drinkRecomendation[1].strDrink }
-              className="d-block w-100"
-            />
-          </div>
+      {drinkRecomendation.map((drink, index) => (
+        <div key={ drink.strDrink } data-testid={ `${index}-recomendation-card` }>
+          <img src={ drink.strDrinkThumb } alt={ drink.strDrink } />
+          <p>{drink.strAlcoholic}</p>
+          <h3>{drink.strDrink}</h3>
         </div>
-        <a
-          className="carousel-control-prev"
-          href="#carouselExampleControls"
-          role="button"
-          data-slide="prev"
-        >
-          <span className="carousel-control-prev-icon" aria-hidden="true" />
-          <span className="sr-only">Anterior</span>
-        </a>
-        <a
-          className="carousel-control-next"
-          href="#carouselExampleControls"
-          role="button"
-          data-slide="next"
-        >
-          <span className="carousel-control-next-icon" aria-hidden="true" />
-          <span className="sr-only">Próximo</span>
-        </a>
-      </div> */}
+      ))}
+      {/* {drinkRecomendation.length && (
+        <CarouselRecomendation drink={ drinkRecomendation } />
+      )} */}
 
       {handleButton()}
     </div>
