d93ae627bbe225da1a8e9b626d4e822cfcc4314b
diogonasci <diogonasci@hotmail.com>
2022-04-02 13:16:30 -0300

    prettier wins

--- src/components/StartRecipe.jsx
+++ src/components/StartRecipe.jsx
@@ -4,33 +4,30 @@ import { string } from 'prop-types';
 
 export default function StartRecipe({ id, type }) {
   const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes')) || [];
-  const inProgressRecipes = JSON.parse(localStorage.getItem('inProgressRecipes'))
-  || { cocktails: {}, meals: {} };
+  const inProgressRecipes = JSON.parse(
+    localStorage.getItem('inProgressRecipes'),
+  ) || { cocktails: {}, meals: {} };
   const history = useHistory();
   const cocktails = 'cocktails';
   const meals = 'meals';
 
   const done = doneRecipes.some((recipe) => recipe.id === id);
-  const progressRecipes = Object
-    .keys(inProgressRecipes[type === 'food' ? meals : cocktails])
-    .some((recipe) => recipe.id === id);
+  const progressRecipes = Object.keys(
+    inProgressRecipes[type === 'food' ? meals : cocktails],
+  ).some((recipe) => recipe.id === id);
 
   return (
     <div>
-      { !done
-      && <div>
+      {!done && (
         <button
           style={ { position: 'fixed', bottom: '0px' } }
-          onClick={
-            () => history.push(`/${type}s/${id}/in-progress`)
-          }
+          onClick={ () => history.push(`/${type}s/${id}/in-progress`) }
           type="button"
           data-testid="start-recipe-btn"
         >
-
-          { progressRecipes ? 'Continue Recipe' : 'Start Recipe' }
+          {progressRecipes ? 'Continue Recipe' : 'Start Recipe'}
         </button>
-      </div>}
+      )}
     </div>
   );
 }
